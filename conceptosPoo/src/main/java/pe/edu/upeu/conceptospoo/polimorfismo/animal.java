e454dd064352c552949eee5121ccec5d6c8fc078
modificacion <modificacion>
2025-09-29 08:48:18 -0500

    nueva rama proyectos eliminados

--- conceptosPoo/src/main/java/pe/edu/upeu/conceptospoo/polimorfismo/animal.java
+++
@@ -1,7 +0,0 @@
-package pe.edu.upeu.conceptospoo.polimorfismo;
-
-public class animal {
-    void sonidoanimal(){
-        System.out.println("animal emite un sonido");
-    }
-}
--- conceptosPoo/src/main/java/pe/edu/upeu/conceptospoo/polimorfismo/main.java
+++
@@ -1,8 +0,0 @@
-package pe.edu.upeu.conceptospoo.polimorfismo;
-
-public class main {
-    public static void main(String[] args) {
-        animal animal = new animal();
-        animal.sonidoanimal();
-    }
-}
